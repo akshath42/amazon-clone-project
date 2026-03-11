fc466224e43bddcb0af15e17fcf9788feaf1facc
Akshath <akshathkhanna@gmail.com>
2026-03-11 12:47:47 +0530

    load products from backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,10 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
+import { loadProducts } from "../data/products.js";
 
-import '../data/backendPractice.js';
-
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+})
