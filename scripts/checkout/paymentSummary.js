9c37efde8c9cd499761d708c60e627c489c9999e
Akshath <akshathkhanna@gmail.com>
2026-03-13 15:00:03 +0530

    loaded orders from backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,6 +1,7 @@
 import { cart, getDeliveryOption, getProduct } from "../../data/cart.js";
 import { formatCurrency } from "../utils/money.js";
 import { cartQuantity } from "../../data/cart.js";
+import { addOrder } from "../../data/order.js";
 
 export function renderPaymentSummary () {
     //Cost In cents
@@ -52,10 +53,31 @@ export function renderPaymentSummary () {
             <div class="payment-summary-money">$${formatCurrency(orderTotal)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order-button">
             Place your order
           </button>
         `;
     document.querySelector('.js-payment-summary')
         .innerHTML = paymentHtml;
+
+    document.querySelector('.js-place-order-button')
+      .addEventListener('click', async () => {
+        try {
+            const response = await fetch('https://supersimplebackend.dev/orders',  {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              cart
+            })
+          });
+          const order = await response.json();
+          addOrder(order);
+        }
+        catch (error) {
+          console.log('Unexpected error')
+        }
+        window.location.href = 'orders.html'
+      })
 }
